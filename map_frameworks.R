4ef9571a43a3cb0259ad74d187148346f9e729dd
Alexis Athens <lexisathens1@gmail.com>
2026-02-03 18:04:13 -0300

    Read in key frameworks (starting with FDES and GS)

--- map_frameworks.R
+++ map_frameworks.R
@@ -2,3 +2,10 @@ library(tidyverse)
 library(readxl)
 library(magrittr)
 library(here)
+
+## Read in framework implementation files:
+# ESSAT > Framework for the Development of Environment Statistics (FDES) > found at https://unstats.un.org/unsd/envstats/fdes/essat.cshtml
+# CISAT > Global Set of Climate Change Statistics and Indicators (GS) > found at https://unstats.un.org/unsd/envstats/Climate%20Change/cisat.cshtml
+
+fdes <- read_excel(here("Frameworks/PartII_ESSAT.xlsm"), sheet = "Component 1")
+gs <- read_excel(here("Frameworks/CISAT_Part_2.xlsx"), sheet = "Self assessment tool")
